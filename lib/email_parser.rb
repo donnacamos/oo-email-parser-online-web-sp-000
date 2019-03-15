e0a5eb6c277817f952c41aa4a9a073662630c130
Donna Amos <donnacamos.developer@gmail.com>
2019-03-15 02:23:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,5 +10,6 @@ class EmailParser
   
   def parse 
     email_array = @email.split(/[, ]/).uniq 
-    email_array.reject! {|element| element.
+    email_array.reject! {|element| element.empty?} 
+    
 end 
